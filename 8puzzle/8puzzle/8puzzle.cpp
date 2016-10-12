66bbea9a222d89fee6213c74ecea9f31a8257169
MSDN-SPECIAL\Administrator <pkpkpkooo@naver.com>
2016-10-13 00:22:21 +0900

    구현완료, dfs a* 실패조건 재설정필요

--- 8puzzle/8puzzle/8puzzle.cpp
+++ 8puzzle/8puzzle/8puzzle.cpp
@@ -18,11 +18,24 @@ struct zero_position {
 };
 struct puzzle {
 	int evalue;
+	int dist;
 	int panel[3][3];
 	zero_position zp;
 	puzzle *parent;
 	list<puzzle> child;
 };
+
+//hamming 함수
+int hamming(puzzle a, puzzle b) {
+	int point = 0;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (a.panel[i][j] == b.panel[i][j])
+				point++;
+		}
+	}
+	return point;
+}
 
 //퍼즐출력함수
 void print_puzzle(puzzle p) {
@@ -33,6 +46,7 @@ void print_puzzle(puzzle p) {
 		}
 		cout << endl;
 	}
+	cout << "평가함수 값 : " << p.evalue << endl;
 }
 
 //start와 goal 퍼즐 초기화함수
@@ -50,6 +64,7 @@ void init_puzzle(puzzle *p, FILE *f) {
 		}
 		cout << endl;
 	}
+	
 }
 
 //풀이과정 출력함수
@@ -63,17 +78,6 @@ void print_answer(vector <puzzle> sol) {
 		std::cout << endl;
 	}
 }
-//hamming 함수
-int hamming(puzzle a, puzzle b) {
-	int point = 0;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (a.panel[i][j] == b.panel[i][j])
-				point++;
-		}
-	}
-	return point;
-}
 
 //child 노드 생성함수, 0위치기준 노드생성순서는 좌 상 우 하 순서
 void make_nodes(puzzle* p) {
@@ -85,7 +89,7 @@ void make_nodes(puzzle* p) {
 		memcpy(tmp.panel, p->panel, sizeof(int) * 9);	//tmp의 panel로 값을 복사한 후 0의 위치를 왼쪽으로이동한다
 		tmp.panel[p->zp.i][p->zp.j] = p->panel[p->zp.i][p->zp.j - 1];
 		tmp.panel[p->zp.i][p->zp.j - 1] = p->panel[p->zp.i][p->zp.j];
-
+		tmp.dist = p->dist + 1;	//자식은 부모보다 거리가중치가 1 많아진다.
 		while (parent != NULL) {		//부모를 거슬러올라가며 hamming으로 중복된 상태인지 검사
 			if (9 > hamming(*parent, tmp))
 				parent = parent->parent;
@@ -107,7 +111,7 @@ void make_nodes(puzzle* p) {
 		memcpy(tmp.panel, p->panel, sizeof(int) * 9);	//tmp의 panel로 값을 복사한 후 0의 위치를 왼쪽으로이동한다
 		tmp.panel[p->zp.i][p->zp.j] = p->panel[p->zp.i - 1][p->zp.j];
 		tmp.panel[p->zp.i - 1][p->zp.j] = p->panel[p->zp.i][p->zp.j];
-
+		tmp.dist = p->dist + 1;	//자식은 부모보다 거리가중치가 1 많아진다.
 		while (parent != NULL) {		//부모를 거슬러올라가며 hamming으로 중복된 상태인지 검사
 			if (9 > hamming(*parent, tmp))
 				parent = parent->parent;
@@ -129,7 +133,7 @@ void make_nodes(puzzle* p) {
 		memcpy(tmp.panel, p->panel, sizeof(int) * 9);	//tmp의 panel로 값을 복사한 후 0의 위치를 왼쪽으로이동한다
 		tmp.panel[p->zp.i][p->zp.j] = p->panel[p->zp.i][p->zp.j + 1];
 		tmp.panel[p->zp.i][p->zp.j + 1] = p->panel[p->zp.i][p->zp.j];
-
+		tmp.dist = p->dist + 1;	//자식은 부모보다 거리가중치가 1 많아진다.
 		while (parent != NULL) {		//부모를 거슬러올라가며 hamming으로 중복된 상태인지 검사
 			if (9 > hamming(*parent, tmp))
 				parent = parent->parent;
@@ -151,7 +155,7 @@ void make_nodes(puzzle* p) {
 		memcpy(tmp.panel, p->panel, sizeof(int) * 9);	//tmp의 panel로 값을 복사한 후 0의 위치를 왼쪽으로이동한다
 		tmp.panel[p->zp.i][p->zp.j] = p->panel[p->zp.i + 1][p->zp.j];
 		tmp.panel[p->zp.i + 1][p->zp.j] = p->panel[p->zp.i][p->zp.j];
-
+		tmp.dist = p->dist + 1;	//자식은 부모보다 거리가중치가 1 많아진다.
 		while (parent != NULL) {		//부모를 거슬러올라가며 hamming으로 중복된 상태인지 검사
 			if (9 > hamming(*parent, tmp))
 				parent = parent->parent;
@@ -175,10 +179,12 @@ void DFS(puzzle start, puzzle goal) {
 	cout << "start DFS" << endl;
 	stack<puzzle> OPEN;
 	OPEN.push(start);
-	while (OPEN.size() > 0 && end_t - start_t < 5) {
+	do{
+		//end_t = clock();
 		if (OPEN.top().child.size() == 0)
 			make_nodes(&OPEN.top());
-		if (hamming(OPEN.top(), goal) < 8) {	//hamming의 결과 point가 8보다 작으면(goal과 일치하지 않으면) 다음노드를 OPEN에 push
+		OPEN.top().evalue = hamming(OPEN.top(), goal);
+		if (OPEN.top().evalue < 8) {	//hamming의 결과 point가 8보다 작으면(goal과 일치하지 않으면) 다음노드를 OPEN에 push
 			if (OPEN.size() < 6) {	//깊이를 5로 제한한다.
 				OPEN.push(OPEN.top().child.front());
 			}else {
@@ -198,7 +204,7 @@ void DFS(puzzle start, puzzle goal) {
 			print_answer(sol);
 			return;
 		}
-	}
+	}while (OPEN.size() > 0 && start.child.size() > 0);
 	cout << "답을 찾지 못하였습니다. 걸린시간 : " << end_t - start_t  << " 초"<< endl;
 }
 
@@ -211,10 +217,11 @@ void BFS(puzzle start, puzzle goal) {
 	OPEN.push_back(start);
 
 	while(OPEN.size() > 0 && end_t - start_t < 5 ){	//OPEN에 더이상 노드가 없거나, 노드의 갯수가 천개가 넘어가면 종료
-		
-		if (hamming(OPEN.front(), goal) < 8) {	//OPEN의 가장 앞에 있는 노드를 목표와 비교해서 일치하지 않으면 
+		//end_t = clock();
+		OPEN.front().evalue = hamming(OPEN.front(), goal);
+		if (OPEN.front().evalue < 8) {	//OPEN의 가장 앞에 있는 노드를 목표와 비교해서 일치하지 않으면 
 			CLOSE.push_back(OPEN.front());		//OPEN의 첫번쨰 노드를 CLOSE로 복사하고 pop한다
-			OPEN.pop_front();	
+			OPEN.pop_front();	 
 			make_nodes(&CLOSE.back());			//CLOSE의 마지막 노드에게 자식노드를 만들어준다
 			list<puzzle>::iterator it;
 			for (it = CLOSE.back().child.begin(); it != CLOSE.back().child.end(); it++) {
@@ -240,13 +247,13 @@ bool pred(puzzle a, puzzle b) { return  a.evalue < b.evalue ? true : false; }
 //언덕오르기 탐색
 void HillClimbing(puzzle start, puzzle goal, int ef(puzzle a, puzzle b)){
 	start_t = clock();
-
 	cout << "start Hill climbing" << endl;
 	list<puzzle> OPEN;
 	list<puzzle> CLOSE;
 	OPEN.push_back(start);
 
-	while(OPEN.size() > 0  && end_t - start_t < 5){
+	while(OPEN.size() > 0 ){
+		end_t = clock();
 		CLOSE.push_back(OPEN.front());
 		make_nodes(&CLOSE.back());
 		OPEN.pop_front();
@@ -254,10 +261,9 @@ void HillClimbing(puzzle start, puzzle goal, int ef(puzzle a, puzzle b)){
 		if(CLOSE.back().child.size() > 0){	//CLOSE에 들어간 원소에 child가 존재하면
 
 			list<puzzle>::iterator it;
-			for(it = CLOSE.back().child.begin() ; it != CLOSE.back().child.end(); it++){	//각 child의 평가함수점수를 구한다.
-				if(ef(*it, goal) < 8) {		//평가함수의 점수가 8미만이면 저장함
-					it->evalue = ef(*it, goal);	
-				} else {					//평가함수의 점수가 8이상이면 정답
+			for(it = CLOSE.back().child.begin() ; it != CLOSE.back().child.end(); it++){	//각 child의 평가함수점수를 구한다
+				it->evalue = ef(*it, goal);		//평가함수의 점수를 저장함
+				if(it->evalue >= 8) {			//평가함수의 점수가 8이상이면 정답					
 					vector<puzzle> sol;
 					puzzle *tmp = &*it;
 					do {
@@ -274,17 +280,117 @@ void HillClimbing(puzzle start, puzzle goal, int ef(puzzle a, puzzle b)){
 				OPEN.push_front(*it);
 			}
 		}
-	
 	}
 
 }
 
 //최적우선 탐색
-void BestFirstSearch(puzzle start, puzzle goal){
+void BestFirstSearch(puzzle start, puzzle goal, int ef(puzzle a, puzzle b)) {
+	start_t = clock();
+	cout << "start BestFirstSearch" << endl;
+	list<puzzle> OPEN;
+	list<puzzle> CLOSE;
+	OPEN.push_back(start);
+
+	while (OPEN.size() > 0) {
+		end_t = clock();
+		CLOSE.push_back(OPEN.back());
+		make_nodes(&CLOSE.back());
+		OPEN.pop_back();
+		if (CLOSE.back().child.size() > 0) {	//CLOSE에 들어간 원소에 child가 존재하면
+			list<puzzle>::iterator it;
+			for (it = CLOSE.back().child.begin(); it != CLOSE.back().child.end(); it++) {	//각 child의 평가함수점수를 구한다
+				it->evalue = ef(*it, goal);		//평가함수의 점수를 저장함
+				if (it->evalue >= 8) {			//평가함수의 점수가 8이상이면 정답					
+					vector<puzzle> sol;
+					puzzle *tmp = &*it;
+					do {
+						sol.push_back(*tmp);
+						tmp = tmp->parent;
+					} while (tmp != NULL);
+					reverse(sol.begin(), sol.end());
+					print_answer(sol);
+					return;
+				}
+			}
+			for (it = CLOSE.back().child.begin(); it != CLOSE.back().child.end(); it++) {		//child들을 OPEN에 삽입
+				OPEN.push_back(*it);
+			}
+			OPEN.sort(pred);	//OPEN을 정렬하여 가장 큰값을 마지막으로 둔다
+		}
+	}
+}
+
+bool Astar_pred(puzzle a, puzzle b) { return  a.evalue - a.dist < b.evalue - b.dist ? true : false; }	//노드->목표 까지의 예상경로비용 - 루트->노드 까지의 경로비용 순으로 정렬 
+
+void reset_dist(puzzle *a){		//입력받은 노드의 자식노드들의 dist를 입력받은 노드의 dist를 기준으로 재설정한다
+	if(a->child.size()){
+		for (list<puzzle>::iterator it = a->child.begin(); it != a->child.begin(); it++){
+			it->dist = a->dist + 1;
+			reset_dist(&*it);
+		}
+	}
+		return;
+	
 }
 
 //A* 탐색
-void AStar(puzzle start, puzzle goal){
+void AStar(puzzle start, puzzle goal, int ef(puzzle a, puzzle b)) {
+	start_t = clock();
+	cout << "start A* algorithm" << endl;
+	list<puzzle> OPEN;
+	list<puzzle> CLOSE;
+	OPEN.push_back(start);
+
+	while (OPEN.size() > 0) {
+		end_t = clock();
+		CLOSE.push_back(OPEN.back());
+		make_nodes(&CLOSE.back());
+		OPEN.pop_back();
+		if (CLOSE.back().child.size() > 0) {	//CLOSE에 들어간 원소에 child가 존재하면
+			list<puzzle>::iterator it;
+			for (it = CLOSE.back().child.begin(); it != CLOSE.back().child.end(); it++) {	//각 child의 평가함수점수를 구한다
+				it->evalue = ef(*it, goal);		//평가함수의 점수를 저장함
+				if (it->evalue >= 8) {			//평가함수의 점수가 8이상이면 정답					
+					vector<puzzle> sol;
+					puzzle *tmp = &*it;
+					do {
+						sol.push_back(*tmp);
+						tmp = tmp->parent;
+					} while (tmp != NULL);
+					reverse(sol.begin(), sol.end());
+					print_answer(sol);
+					return;
+				}
+			}
+			for (it = CLOSE.back().child.begin(); it != CLOSE.back().child.end(); it++) {		//child들을 OPEN에 삽입
+				for (list<puzzle>::iterator oit = OPEN.begin(); oit != OPEN.end(); oit++) {		//OPEN에 중복되는 노드가 있는지 검사
+					if (hamming(*it, *oit) >= 8) {		//중복되는 경우가 있다면
+						if (it->evalue - it->dist > oit->evalue - oit->dist) 	//child의 g() + h()가 더 큰경우에는 OPEN에 있는 중복노드를삭제
+							OPEN.erase(oit--);						
+						else												//그외의 경우에는 child에 있는 중복노드를 삭제
+							CLOSE.back().child.erase(it--);						
+					}
+				}
+				for (list<puzzle>::iterator cit = CLOSE.begin(); cit != CLOSE.end(); cit++) {		//CLOSE에 중복되는 노드가 있는지 검사
+					if (hamming(*it, *cit) >= 8) {		//중복되는 경우가 있다면
+						if (it->evalue - it->dist > cit->evalue - cit->dist) {	//child의 g() + h()가 더 큰경우에는 CLOSE에 있는 중복노드를삭제
+							*cit->parent = *it->parent;
+							cit->dist = it->dist;
+							reset_dist(&*cit);
+							CLOSE.back().child.erase(it--);
+						}else {													//그외의 경우에는 child에 있는 중복노드를 삭제
+							CLOSE.back().child.erase(it--);
+						}
+					}
+
+				}
+				OPEN.push_back(*it);
+			}
+			
+		}
+		OPEN.sort(Astar_pred);	//OPEN을 정렬하여 g() + h()의 값이 가장 큰노드를 마지막으로 둔다
+	}
 
 }
 
@@ -314,11 +420,14 @@ int main(int argc, char* argv[])
 	cout << "zero position : i = " << goal.zp.i << ", j = " << goal.zp.j << endl;
 	printf("\n");
 	start.parent = NULL;
+	start.evalue = hamming(start, goal);
+	start.dist = 0;
 
 	DFS(start, goal);
-	BFS(start, goal);
-	HillClimbing(start, goal, hamming);
-
+	//BFS(start, goal);
+	//HillClimbing(start, goal, hamming);
+	//BestFirstSearch(start, goal, hamming);
+	//AStar(start, goal, hamming);
 
 	fclose(in);
 	fclose(g);
